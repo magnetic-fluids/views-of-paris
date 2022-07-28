87dfcd637a66378b2d4306d571ace9697fffaafa
Charles Van West <charles@vanwestco.com>
2022-07-28 11:24:50 -0400

    Use grouping for magnetics data and its field vectors

--- magnetic_drop.py
+++ magnetic_drop.py
@@ -55,61 +55,65 @@ for source_list in vtk_sources:
 
 # read all magnetics files and organize by timestep
 input_files = sorted(input_path.glob('mag00000*.vtk'))
-
-# assuming all the files are present and formatted as VOFstep-process.vtk...
-def file_to_idxs(file):
-	return map(int, re.findall(r'\d+', file.name))
-
-[steps, processes] = file_to_idxs(input_files[-1])
-print(f'{steps} steps, {processes} processes')
-# preallocate list of sources
 mag_sources = [[None] * (processes + 1)] * (steps + 1)
 
 for file in input_files:
 	source = LegacyVTKReader(registrationName=file.name, FileNames=[str(file)])
-	display = GetDisplayProperties(source)
 	[step, process] = file_to_idxs(file)
-	mag_sources[step][process] = (source, display)
-	Hide(source, main_view)
+	mag_sources[step][process] = source
 
-	# set up magnetics coloring
-	display.SetRepresentationType('Wireframe')
-	ColorBy(display, ('CELLS', 'PHIMAG'))
-	display.RescaleTransferFunctionToDataRange(True, False)
-	display.SetScalarBarVisibility(main_view, True)
-	pHIMAGLUT = GetColorTransferFunction('PHIMAG')
-
-	# get the gradient of the magnetic potential
-	gradient = Gradient(registrationName=file.name + '_gradient', Input=source)
-	gradient.ScalarArray = ['CELLS', 'PHIMAG']
-	gradient_display = Show(gradient, main_view, 'StructuredGridRepresentation')
-	Hide(gradient, main_view)
-
-	# show a collection of field vectors
-	field_vecs = Glyph(registrationName=file.name + '_field_vecs', Input=gradient, GlyphType='Arrow')
-	field_vecs.GlyphTransform = 'Transform2'
+# merge magnetics data by timestep and render some fun stuff
+for source_list in mag_sources:
+	dataset_group = GroupDatasets(Input=source_list)
+	potential = MergeBlocks(Input=dataset_group)
+	field = Gradient(Input=potential)
+	field.ScalarArray = ['CELLS', 'PHIMAG']
+	field_vecs = Glyph(Input=field, GlyphType='Arrow')
 	field_vecs.OrientationArray = ['CELLS', 'Gradient']
 	field_vecs.ScaleArray = ['CELLS', 'Gradient']
-	field_vecs.ScaleFactor = 5.7471208929013815e-06
+	field_vecs.ScaleFactor = 6e-6
 	field_vecs.MaximumNumberOfSamplePoints = 200
+	Show(field_vecs, main_view)
 
-	field_vecs_display = Show(field_vecs, main_view, 'GeometryRepresentation')
-	field_vecs_display.SetScalarBarVisibility(main_view, True)
-
-	field_lines = StreamTracer(registrationName=file.name + '_field_lines', Input=gradient, SeedType='Line')
-	field_lines.Vectors = ['CELLS', 'Gradient']
-	field_lines.MaximumStreamlineLength = 0.02
-
-	# init the 'Line' selected for 'SeedType'
-	#field_lines.SeedType.Point1 = [0.02, 0.02, 0.02]
-	#field_lines.SeedType.Point2 = [0.04, 0.04, 0.04]
-	field_lines.SeedType.Resolution = 200
-
-	# show data in view
-	field_lines_display = Show(field_lines, main_view, 'GeometryRepresentation')
-	ColorBy(field_lines_display, ('POINTS', 'Gradient', 'Magnitude'))
-	field_lines_display.RescaleTransferFunctionToDataRange(True, False)
-	field_lines_display.SetScalarBarVisibility(main_view, True)
+#
+#	# set up magnetics coloring
+#	display.SetRepresentationType('Wireframe')
+#	ColorBy(display, ('CELLS', 'PHIMAG'))
+#	display.RescaleTransferFunctionToDataRange(True, False)
+#	display.SetScalarBarVisibility(main_view, True)
+#	pHIMAGLUT = GetColorTransferFunction('PHIMAG')
+#
+#	# get the gradient of the magnetic potential
+#	gradient = Gradient(registrationName=file.name + '_gradient', Input=source)
+#	gradient.ScalarArray = ['CELLS', 'PHIMAG']
+#	gradient_display = Show(gradient, main_view, 'StructuredGridRepresentation')
+#	Hide(gradient, main_view)
+#
+#	# show a collection of field vectors
+#	field_vecs = Glyph(registrationName=file.name + '_field_vecs', Input=gradient, GlyphType='Arrow')
+#	field_vecs.GlyphTransform = 'Transform2'
+#	field_vecs.OrientationArray = ['CELLS', 'Gradient']
+#	field_vecs.ScaleArray = ['CELLS', 'Gradient']
+#	field_vecs.ScaleFactor = 5.7471208929013815e-06
+#	field_vecs.MaximumNumberOfSamplePoints = 200
+#
+#	field_vecs_display = Show(field_vecs, main_view, 'GeometryRepresentation')
+#	field_vecs_display.SetScalarBarVisibility(main_view, True)
+#
+#	field_lines = StreamTracer(registrationName=file.name + '_field_lines', Input=gradient, SeedType='Line')
+#	field_lines.Vectors = ['CELLS', 'Gradient']
+#	field_lines.MaximumStreamlineLength = 0.02
+#
+#	# init the 'Line' selected for 'SeedType'
+#	#field_lines.SeedType.Point1 = [0.02, 0.02, 0.02]
+#	#field_lines.SeedType.Point2 = [0.04, 0.04, 0.04]
+#	field_lines.SeedType.Resolution = 200
+#
+#	# show data in view
+#	field_lines_display = Show(field_lines, main_view, 'GeometryRepresentation')
+#	ColorBy(field_lines_display, ('POINTS', 'Gradient', 'Magnitude'))
+#	field_lines_display.RescaleTransferFunctionToDataRange(True, False)
+#	field_lines_display.SetScalarBarVisibility(main_view, True)
 
 # show data in view
 def show_timestep(source_list, timestep):
